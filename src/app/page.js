6c0a7b22334df2fdcc1cc1c183fd6d5be1e0b266
demandsupply <giova.zol92@gmail.com>
2026-03-29 18:06:18 +0200

    feat: added section three

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import HeroSection from "@/components/home/HeroSection/HeroSection";
 import SectionTwo from "@/components/home/SectionTwo/SectionTwo";
+import SectionThree from "@/components/home/SectionThree/SectionThree";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className={styles.main}>
         <HeroSection title="PROVA" image="/home/sample-img.webp" />
         <SectionTwo />
+        <SectionThree />
       </main>
     </div>
   );
---
+++ src/components/home/SectionThree/SectionThree.jsx
@@ -0,0 +1,13 @@
+import ImageCard from "@/components/ImageCard/ImageCard"
+import styles from "./SectionThree.module.css"
+
+export default function SectionThree( { title, subtitle }) {
+  return(
+    <section className={styles.sectionThree}>
+      <h2 className={styles.title}>PORTFOLIO</h2>
+      <div className={styles.imagesGrid}>
+          <ImageCard src="/home/sample-img-horizontal.jpg" orientation="horizontal" />
+      </div>
+    </section>
+  )
+}
