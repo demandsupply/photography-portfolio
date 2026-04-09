9e60ec0c20befd11c66ffd7591f88d842e0a2e61
demandsupply <giova.zol92@gmail.com>
2026-04-09 17:37:27 +0200

    fix: changed grid to flex and added three columns to fix issue where images dind't align close to each other

--- src/components/home/SectionThree/SectionThree.jsx
+++ src/components/home/SectionThree/SectionThree.jsx
@@ -4,17 +4,26 @@ import styles from "./SectionThree.module.css"
 export default function SectionThree( { title, subtitle }) {
   return(
     <section className={styles.sectionThree}>
-      <h2 className={styles.title}>PORTFOLIO</h2>
+      <h2 className={styles.title}>SELECTED WORKS</h2>
       <div className={styles.imagesGrid}>
+        <div className={styles.column}>
           <ImageCard src="/home/sample-img-horizontal.jpg" orientation="horizontal" />
           <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
           <ImageCard src="/home/sample-img-horizontal.jpg" orientation="horizontal" />
           <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
-          <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
+        </div>
+        <div className={styles.column}>
           <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
           <ImageCard src="/home/sample-img-horizontal.jpg" orientation="horizontal" />
+          <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
+          <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
+        </div>
+        <div className={styles.column}>
           <ImageCard src="/home/sample-img-horizontal.jpg" orientation="horizontal" />
           <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
+          <ImageCard src="/home/sample-img-horizontal.jpg" orientation="horizontal" />
+          <ImageCard src="/home/sample-img-vertical.jpg" orientation="vertical" />
+        </div>
       </div>
     </section>
   )
