f4c29f594a9aa5de5c5dd009522d5b067eb6238c
demandsupply <giova.zol92@gmail.com>
2026-04-17 18:28:34 +0200

    style: toggle header transparency based on route ( if the user is on the homepage or not)

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import styles from "./Header.module.css"
 
 export default function Header() {
+    const pathname = usePathname();
+    const isHome = pathname === "/";
+    const headerClass = `${styles.headerContainer} ${isHome ? styles.headerHome : styles.headerPages}`
     return (
-        <header className={styles.headerContainer}>
+        <header className={headerClass}>
             <Navbar />
         </header>
     )
