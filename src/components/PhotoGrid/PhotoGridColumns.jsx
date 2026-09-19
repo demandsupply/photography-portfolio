6da27c136f6521b15c3044a304101617f9f08203
demandsupply <giova.zol92@gmail.com>
2026-09-19 15:04:13 +0200

    refactor: update PhotogridColumns component to accept customized breakpoints, replaced PhotoGridRows component on homepage with it

--- src/components/PhotoGrid/PhotoGridColumns.jsx
+++ src/components/PhotoGrid/PhotoGridColumns.jsx
@@ -4,24 +4,53 @@ import ImageCard from "../ImageCard/ImageCard";
 import styles from "./PhotoGrid.module.css";
 import JSONPhotos from "@/data/photos.json";
 
-export default function PhotoGridColumns({ categoryName }) {
+const DEFAULT_BREAKPOINTS = {
+  480: 1,
+  768: 2,
+  1024: 3,
+  default: 4
+}
+
+export default function PhotoGridColumns({ categoryName, photos, breakpoints, className="" }) {
   const PHOTOS = JSONPhotos;
 
-  const [totalColumns, setTotalColumns] = useState(3);
+  // fetch photos from an array of pictures OR from JSON["categoryName"]
+  const photosToShow = photos ?? (categoryName ? PHOTOS[categoryName] : []);
+
+  // merge default breakpoints with props breakpoints
+  const updatedBreakpoints = { ...DEFAULT_BREAKPOINTS, ...breakpoints };
+
+  let [totalColumns, setTotalColumns] = useState(updatedBreakpoints.default ?? 3);
 
   useEffect(() => {
+    // calculate thresholds only once
+    const thresholds = Object.keys(updatedBreakpoints).filter((key) => key !== "default").map(Number).sort((a, b) => a - b);
+    
     const handleColumnsNumber = () => {
       const width = innerWidth;
-      if (width < 480) setTotalColumns(1);
-      else if (width < 768) setTotalColumns(2);
-      else if (width < 1024) setTotalColumns(3);
-      else setTotalColumns(4);
+      // if (width < 480) setTotalColumns(1);
+      //else if (width < 768) setTotalColumns(2);
+      // else if (width < 1024) setTotalColumns(3);
+      // else setTotalColumns(4);
+      
+      let foundColumns = updatedBreakpoints.default ?? 3;
+      
+      for (const threshold of thresholds) {
+        if (width <= threshold) {
+          foundColumns = updatedBreakpoints[threshold];
+          break;
+        }
+      }
+      setTotalColumns(foundColumns)
     };
 
     handleColumnsNumber();
-
     window.addEventListener("resize", handleColumnsNumber);
-  });
+
+    return () => window.removeEventListener("resize", handleColumnsNumber)
+    // [] execute once, when the componet is mounted
+  }, []);
+
 
   const columns = useMemo(() => {
     // create n empty arrays
@@ -33,7 +62,7 @@ export default function PhotoGridColumns({ categoryName }) {
     const columnHeights = Array.from({ length: totalColumns }, () => 0);
     console.log(columnHeights);
 
-    PHOTOS[categoryName].map((element) => {
+    photosToShow.map((element) => {
       const shortestColumnIndex = columnHeights.indexOf(
         Math.min(...columnHeights),
       );
@@ -45,11 +74,11 @@ export default function PhotoGridColumns({ categoryName }) {
     });
 
     return result;
-  }, [categoryName, totalColumns, PHOTOS]);
+  }, [photosToShow, totalColumns]);
 
   return (
     <div
-      className={styles.photoGridColumsContainer}
+      className={`${styles.photoGridColumsContainer} ${className}`}
       style={{ gridTemplateColumns: `repeat(${totalColumns}, 1fr )` }}
     >
       {columns.map((column, index) => (
--- src/components/home/SectionThree/SectionThree.jsx
+++ src/components/home/SectionThree/SectionThree.jsx
@@ -1,19 +1,26 @@
 import ImageCard from "@/components/ImageCard/ImageCard";
 import styles from "./SectionThree.module.css";
 import PhotoGridRows from "@/components/PhotoGrid/PhotoGridRows";
+import PhotoGridColumns from "@/components/PhotoGrid/PhotoGridColumns";
 import selectedPhotos from "@/data/selectedPhotos";
 
 
 export default function SectionThree({ title, subtitle }) {
+  const customBreakponts = {
+    480: 1,
+    768: 2,
+    default: 3
+  }
   return (
     <section className={styles.sectionThree}>
       <h2 className={styles.title}>Selected Works</h2>
       {/* ROWS GRID */}
-      <PhotoGridRows photos={selectedPhotos}/>
+      {/* <PhotoGridRows photos={selectedPhotos} className={styles.imagesGrid}/> */}
 
 
       {/* COLUMNS GRID */}
-      {/* <div className={styles.imagesGridColumns}>
+      <PhotoGridColumns photos={selectedPhotos} breakpoints={customBreakponts}/>
+      {/* <div className={styles.imagesGrid}>
         <div className={styles.column}>
           <ImageCard
             src="/home/sample-img-horizontal.jpg"
